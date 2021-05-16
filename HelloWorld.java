e334843f163414803f9dafc7dbc2b3feafa9a6e4
Chou <maychoutw9@gmail.com>
2021-05-16 15:10:30 +0800

    add animation

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld
     public static void main(String[] args) {
     
         System.out.println("Hello~TFA101的各位!!");
+		System.out.println("HelloWorld");
+		System.out.println("HW");
 
     }
 
